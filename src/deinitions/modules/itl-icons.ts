95a17340ad79ba8b0d9ee458e20e672ec26e6bdd
Данил Степин <dstepin@itoolabs.com>
2026-02-28 12:06:55 +0300

    build 4

--- src/deinitions/modules/itl-icons.ts
+++ src/deinitions/modules/itl-icons.ts
@@ -1,3 +1,3 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-export { default as itlIcons } from "@ad/itl-icons"
+export { default as itlIcons } from "@ad/itl-icons.js"
